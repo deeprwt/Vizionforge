c41dd57a8ac21101e96a0cb7005c95f05ecca3ca
deeprwt <rwtroshan28@gmail.com>
2026-03-22 21:11:44 +0530

    fix: update deploy workflow and server.js

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
-const path = require("path");
+const { createServer } = require("http");
+const { parse } = require("url");
+const next = require("next");
 
-// Set the directory for Next.js standalone
-process.chdir(path.join(__dirname, ".next", "standalone"));
+const dev = false;
+const hostname = "0.0.0.0";
+const port = process.env.PORT || 3000;
 
-// Start the standalone server
-require("./.next/standalone/server.js");
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    try {
+      const parsedUrl = parse(req.url, true);
+      await handle(req, res, parsedUrl);
+    } catch (err) {
+      console.error("Error occurred handling", req.url, err);
+      res.statusCode = 500;
+      res.end("internal server error");
+    }
+  }).listen(port, () => {
+    console.log(`> Ready on http://${hostname}:${port}`);
+  });
+});
