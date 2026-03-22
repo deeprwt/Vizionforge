b6a7e91d568157c25cb8fa01f6b46faba28b7bdf
deeprwt <rwtroshan28@gmail.com>
2026-03-23 01:22:16 +0530

    update new files

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const path = require("path");
 const { createServer } = require("http");
 const { parse } = require("url");
-const next = require("next");
 
-const app = next({ dev: false, dir: __dirname });
+const dir = path.join(__dirname);
+process.env.NODE_ENV = "production";
+process.chdir(__dirname);
+
+const nextConfigStr = require("fs").readFileSync(
+  path.join(__dirname, ".next", "required-server-files.json"),
+  "utf8"
+);
+const { config: nextConfig } = JSON.parse(nextConfigStr);
+process.env.__NEXT_PRIVATE_STANDALONE_CONFIG = JSON.stringify(nextConfig);
+
+const next = require("next");
+const app = next({ dev: false, dir: dir, conf: nextConfig });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
+  const port = parseInt(process.env.PORT, 10) || 3000;
+  const hostname = process.env.HOSTNAME || "0.0.0.0";
   createServer((req, res) => {
     handle(req, res, parse(req.url, true));
-  }).listen(process.env.PORT || 3000, () => {
-    console.log("> Ready");
+  }).listen(port, hostname, () => {
+    console.log("> Ready on http://" + hostname + ":" + port);
   });
 });
